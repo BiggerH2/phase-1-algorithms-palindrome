dd5b8980f9be8430168af340a846cd13ed074860
BiggerH2 <161705584+BiggerH2@users.noreply.github.com>
2024-04-07 13:49:22 +0300

    palindromes

--- index.js
+++ index.js
@@ -1,13 +1,23 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  const reversedWord = word.split("").reverse().join("");
+  return word === reversedWord;
 }
 
 /* 
   Add your pseudocode here
+  - Split the word into individual characters
+  - Reverse the order of the characters
+  - Join the characters back into a string
+  - Check if the original word is equal to the reversed word
 */
 
 /*
   Add written explanation of your solution here
+  - The function splits the input word into individual characters, reverses their order,
+    and then joins them back into a string.
+  - It then compares this reversed string with the original input word to determine
+    if it is a palindrome.
 */
 
 // You can run `node index.js` to view these console logs
